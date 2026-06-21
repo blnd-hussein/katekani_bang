e123e488b10de3d13692c3d11884100aac0c91ce
Blnd-hussen <blndqaladzey12@gmail.com>
2026-06-21 19:22:00 +0300

    feat: update app manifest and add new icon images

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+  modules: ["@wxt-dev/module-react"],
   manifest: {
-    permissions: ['alarms', 'notifications', 'storage']
-  }
+    name: "کاتەکانی بانگ",
+    short_name: "بانگ",
+    description: "کاتەکانی بانگ و زیکرەکان بە زمانی کوردی",
+    version: "1.0.0",
+    permissions: ["alarms", "notifications", "storage"],
+    action: {
+      default_title: "کاتەکانی بانگ",
+    },
+  },
 });
